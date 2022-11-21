bff38d4d6033cabb4e5a2a574f82f907d25672d4
khaireddine-abidi <75070488+khaireddine-abidi@users.noreply.github.com>
2022-11-21 09:57:17 +0100

    Update java.java

--- java.java
+++ java.java
@@ -1,12 +1,8 @@
-public class Test {
 
-   //Crée une nouvelle instance de Test
-   public Test() {
-   }
 
-   //  args: les arguments de la ligne de commande
+  
    public static void main(String[] args) {
       System.out.println("Allo !");
    }
 
-}
+
