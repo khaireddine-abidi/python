2517bfbfb8c1861dfec124a732e5f50dd5f9ba1e
khaireddine-abidi <75070488+khaireddine-abidi@users.noreply.github.com>
2022-11-21 09:49:32 +0100

    Create java.java

---
+++ java.java
@@ -0,0 +1,12 @@
+public class Test {
+
+   //Crée une nouvelle instance de Test
+   public Test() {
+   }
+
+   //  args: les arguments de la ligne de commande
+   public static void main(String[] args) {
+      System.out.println("Allo !");
+   }
+
+}
